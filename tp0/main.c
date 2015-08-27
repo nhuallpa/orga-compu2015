a577df4db1d9cafb19978ba895dd212121e0aee8
huallpa.nestor <huallpa.nestor@gmail.com>
2015-08-27 01:51:15 -0300

    array

--- tp0/main.c
+++ tp0/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, const char* argv[]) {
-  printf("\nA darle atomos\n");
+
+  int* ptr;
+  int n=100;
+
+  ptr = (int*)malloc(n*sizeof(int));
+
+  int i = 0;
+  for (i=0; i<n; i++) {
+	ptr[i] = i;
+  }
+  
+  for (i=0; i<n; i++) {
+	printf("%d, ", ptr[i]);
+  }
+
+  free(ptr);
+  
   return 0;
 }
