2938dbac87dded9b1c22f17d77b246ac57a1a2d9
ariel55 <arielcorreofiuba@gmail.com>
2015-08-28 01:47:55 -0300

    lectura del archivo de entrada, desde el main.c

--- tp0/main.c
+++ tp0/main.c
@@ -2,8 +2,36 @@
 #include <stdlib.h>
 int main(int argc, const char* argv[]) {
 
+
+	// leer archivo de entrada y cargar en una struct
+	
+	FILE *fp = fopen("entrada01.txt", "r");
+
+	if (fp == NULL) {
+	  fprintf(stderr, "Can't open input file in.list!\n");
+	  exit(1);
+	}
+	else
+	{
+		char line[999];
+	    
+	    while( fgets(line,999,fp) )
+	    {
+		   printf("%s",line);
+		}
+    	
+	    fclose(fp);
+	}
+	
+	// hacer post validaciones
+	
+	// multipliacacion de la matriz
+	
+	// imprimir la matriz por stdout
+
+
   int* ptr;
-  int n=100;
+  int n=10;
 
   ptr = (int*)malloc(n*sizeof(int));
 
@@ -15,6 +43,8 @@ int main(int argc, const char* argv[]) {
   for (i=0; i<n; i++) {
 	printf("%d, ", ptr[i]);
   }
+
+	printf("\n" );
 
   free(ptr);
   
