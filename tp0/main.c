3080c205c2f8eac80b59bf00779b8ee4144b307a
fcaldora <facundo.caldora@gmail.com>
2015-08-28 17:29:37 -0300

    Aqui va una implementacion posible para obtener los datos a traves del archivo y cargarlos sobre la matriz. Cosas interesantes a probar: cambiar dimensiones, o valores, en el archivo de texto y corroborar que funciona. Lo dificil es handelear los errores que se puedan producir por archivos que nos ingresen y esten mal. Es decir, que te ingresen un archivo de texto que en ves de decir 2x2 dija ttxfj (cualquier cosa). Asi como esta lo probe con archivos erroneos y el programa termina porque no puede almacenar la memoria, pero creo que esta mal. Ahora igual realizo un comentario mas detallado en el grupo de facebook por cualquier duda

--- tp0/main.c
+++ tp0/main.c
@@ -3,18 +3,19 @@
 
 int main(int argc, const char* argv[]) {
 
-	
+
 	typedef struct {
 	   int cantFil;
 	   int cantCol;
-	   int** datos;
-	} matriz;	
-	
-	
+	   double** datos;
+	} matriz;
+
+
 	matriz m_a;
 	matriz m_b;
 	int i,j;
-	
+    char* equis = ((char*)malloc(sizeof(char)));
+
 	/* se lee el archivo para leer las dimensiones de las matrices */
 	FILE *fp = fopen("entrada01.txt", "r");
 
@@ -25,60 +26,21 @@ int main(int argc, const char* argv[]) {
 	else
 	{
 		char line[999];
-	    
+
 	    while( fgets(line,999,fp) )
 	    {
 	    	// TODO leer las dimensiones de cada una de las filas
 		   	printf("%s",line);
 		}
-		
-		// dimesiones hard-codeadas
-		m_a.cantFil = 2;
-		m_a.cantCol = 2;    	
-		m_b.cantFil = 2;
-		m_b.cantCol = 2;		
-		
+
 	    fclose(fp);
 	}
 
-	/* se aloja memoria para la matriz a */
-	m_a.datos = ((int**)malloc(m_a.cantFil*sizeof(int*)));
-	if(m_a.datos==NULL){
-		printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ A");
-		exit(1);
-	}
 
-	for(i=0;i<m_a.cantFil;i++)
-	{
-		m_a.datos[i] = ((int*)malloc(m_a.cantCol*sizeof(int)));
-		if(m_a.datos[i]==NULL)
-		{
-			printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ A");
-			exit(-1);
-		}	
-	}	
-	
-	/* se aloja memoria para la matriz b */
-	m_b.datos = ((int**)malloc(m_b.cantFil*sizeof(int*)));
-	if(m_b.datos==NULL)
-	{
-		printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ B");
-		exit(1);
-	}
 
-	for(i=0;i<m_b.cantFil;i++)
-	{
-		m_b.datos[i] = ((int*)malloc(m_b.cantCol*sizeof(int)));
-		if(m_b.datos[i]==NULL){
-			printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ A");
-			exit(-1);
-		}	
-	}
-	
-	
-	
+
 	/* se cargan los datos para las 2 matrices desde el archivo*/
-	
+
 	fp = fopen("entrada01.txt", "r");
 	if (fp == NULL) {
 	  fprintf(stderr, "Can't open input file in.list!\n");
@@ -86,57 +48,124 @@ int main(int argc, const char* argv[]) {
 	}
 	else
 	{
-		char line[999];
-	    
-	    while( fgets(line,999,fp) )
+	    while(!feof(fp))
 	    {
-	    	// TODO leer el contenido para las matrices
+            // Se leen los valores desde el archivo de fila columna y separador de ambos
+            fscanf(fp, "%d" , &m_a.cantFil);if(feof(fp))break;  //Break temporario hasta que encontremos el fix
+            fscanf(fp, "%c" , equis);                           //porque por alguna razón no llega el fin de archivo antes
+            fscanf(fp, "%d", &m_a.cantCol);
+
+            // Handlers de archivos mal ingresados
+            if (m_a.cantFil<0){
+                printf("\n\t\tERROR: FILA INGRESADA INVALIDA PARA MATRIZ A");
+                exit(1);
+            }
+            if (m_a.cantCol<0){
+                printf("\n\t\tERROR: COLUMNA INGRESADA INVALIDA PARA MATRIZ A");
+                exit(1);
+            }
+            	/* se aloja memoria para la matriz a */
+            m_a.datos = ((double**)malloc(m_a.cantFil*sizeof(double*)));
+            if(m_a.datos==NULL){
+                printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ A");
+                exit(1);
+            }
+
+            for(i=0;i<m_a.cantFil;i++)
+            {
+                m_a.datos[i] = ((double*)malloc(m_a.cantCol*sizeof(double)));
+                if(m_a.datos[i]==NULL)
+                {
+                    printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ A");
+                    exit(-1);
+                }
+            }
+
+            // Se cargan los datos de la matriz A
+            for(i=0;i<m_a.cantFil;i++){
+                for(j=0;j<m_a.cantCol;j++){
+
+                    fscanf(fp, "%lf", &m_a.datos[i][j]);
+
+                }
+            }
+            // Se leen los valores desde el archivo de fila columna y separador de ambos
+            fscanf(fp, "%d" , &m_b.cantFil);
+            fscanf(fp, "%c" , equis);
+            fscanf(fp, "%d", &m_b.cantCol);
+
+            if (m_b.cantFil<0){
+                printf("\n\t\tERROR: FILA INGRESADA INVALIDA PARA MATRIZ A");
+                exit(1);
+            }
+            if (m_b.cantCol<0){
+                printf("\n\t\tERROR: COLUMNA INGRESADA INVALIDA PARA MATRIZ A");
+                exit(1);
+            }
+
+            /* se aloja memoria para la matriz b */
+            m_b.datos = ((double**)malloc(m_b.cantFil*sizeof(double*)));
+            if(m_b.datos==NULL)
+            {
+                printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ B");
+                exit(1);
+            }
+
+            for(i=0;i<m_b.cantFil;i++)
+            {
+                m_b.datos[i] = ((double*)malloc(m_b.cantCol*sizeof(double)));
+                if(m_b.datos[i]==NULL){
+                    printf("\n\t\tERROR:MEMORIA INSUFICIENTE PARA MATRIZ A");
+                    exit(-1);
+                }
+            }
+
+            // Se cargan los datos de la matriz B
+            for(i=0;i<m_b.cantFil;i++){
+                for(j=0;j<m_b.cantCol;j++){
+
+                    fscanf(fp, "%lf", &m_b.datos[i][j]);
+
+                }
+            }
+
+            // aqui solo un resumen de lo que nos quedaria por hacer una ves
+            // ingresadas las matrices y cargadas en memoria
+            // resultado = multiplicarmatrices(m_a,m_b);
+            // imprimirmatrizresultado(resultado);
+
+
 		}
 
-		// datos hard-coding para la matriz a
-		int num = 0;
-		for(i=0;i<m_a.cantFil;i++){
-			for(j=0;j<m_a.cantCol;j++){
-				m_a.datos[i][j] = num++;
-			}
-		}
-    	
-		// datos hard-coding para la matriz b
-		for(i=0;i<m_b.cantFil;i++){
-			for(j=0;j<m_b.cantCol;j++){
-				m_b.datos[i][j] = num++;
-			}
-		}    	
-    	
-    	
+
+
 		// imprimir las matrices por stdout
 		for(i=0;i<m_a.cantFil;i++){
 			printf("\n|");
 			for(j=0;j<m_a.cantCol;j++){
-				printf("\t%i\t| ",m_a.datos[i][j]);
+				printf("\t%lf\t| ",m_a.datos[i][j]);
 			}
 		}
-		printf("\n");    	
-		
+		printf("\n");
+
 		for(i=0;i<m_b.cantFil;i++){
 			printf("\n|");
 			for(j=0;j<m_b.cantCol;j++){
-				printf("\t%i\t| ",m_b.datos[i][j]);
+				printf("\t%lf\t| ",m_b.datos[i][j]);
 			}
-		}		
+		}
 		printf("\n");
-		printf("Todavia falta tomar las matrices originales desde el archivo de entrada\n");    	    	
-    	
+
 	    fclose(fp);
-	}	
-	
-	
+	}
+
+
 	// hacer post validaciones
-	
+
 	// multipliacacion de la matriz
-	
+
 	// imprimir la matriz resultado por stdout
 
-  
+
   return 0;
 }
