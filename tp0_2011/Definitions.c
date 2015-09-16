950071fcb8bb41c15f0d873c1e4deeff7f95a7a1
ariel55 <arielcorreofiuba@gmail.com>
2015-09-15 21:17:49 -0300

    Informe: version 1.2 falta un parrafo para la instruccion y agregar el codigo c, codigo assembly y enunciado. Se borro la carpeta tp0_2011 que fue mi tp cuando la curse antes

--- tp0_2011/Definitions.c
+++
@@ -1,9 +0,0 @@
-#include "Definitions.h"
-
-int is_regular(const char* filename){
-    struct stat sb;
-    if (stat(filename,&sb)== ERROR){
-     return ERROR;
-    }
-    return S_ISREG(sb.st_mode);
-}
--- tp0_2011/README_PRUEBAS.txt
+++
@@ -1,6 +0,0 @@
-1) Ejecutar:
-	./generar_pruebas_y_res.sh
-2) Copiar las carpetas Casos_de_prueba, salida y salida_Esperada
-(con todo su contenido) a Netbsd.
-2) Dentro de Netbsd Ejecutar:
-	 ./test.sh
