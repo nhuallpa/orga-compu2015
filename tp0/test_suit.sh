a2bffd8931e18039b4735a78aa620c0656b01b1d
ariel55 <arielcorreofiuba@gmail.com>
2015-09-16 18:26:46 -0300

    Pruebas: Puse las pruebas de cantidad de elementos de la matriz invalidos en el script test_suit.sh. Falta ver el codigo para cuando la cantidad de elementos es mayor a la indicada, por ejemplo cuando en una configuracion de 2x2 se encuentra 5 numeros

--- tp0/test_suit.sh
+++ tp0/test_suit.sh
@@ -2,29 +2,38 @@
 echo "========================================================================="
 echo "Comienzo de pruebas"
 echo "========================================================================="
-echo "Test 1: Multiplicacion simple 2x2"
+echo "Test 1: Multiplicacion simple 2x2."
 cat entrada/test1.txt | ./tp0 	
 echo "-------------------------------------------------------------------------"
-echo "Test 2: Multiplicacion media de 8x8"
+echo "Test 2: Multiplicacion media de 8x8."
 cat entrada/test2.txt | ./tp0 	
 echo "-------------------------------------------------------------------------"
-echo "Test 3: Multiplicacion de dos pares de matrices"
+echo "Test 3: Multiplicacion de dos pares de matrices."
 cat entrada/test3.txt | ./tp0 	
 echo "-------------------------------------------------------------------------"
-echo "Test 4: Multiplicacion de matrices con dimensiones incorrectas"
+echo "Test 4: Multiplicacion de matrices con dimensiones incorrectas."
 cat entrada/testDimensionIncorrecta.txt | ./tp0
 echo "-------------------------------------------------------------------------"
-echo "Test 5: Dimension de matriz leida incorrecta"
+echo "Test 5: Dimension de matriz leida incorrecta."
 cat entrada/testElementoIncorrecto1.txt | ./tp0
 echo "-------------------------------------------------------------------------"
-echo "Test 6: Elemento de matriz leida incorrecta"
+echo "Test 6: Elemento de matriz leida incorrecta."
 cat entrada/testElementoIncorrecto2.txt | ./tp0
 echo "-------------------------------------------------------------------------"
-echo "Test 7: Multiplicacion de matrices impares(solo una matriz cargada)"
+echo "Test 7: Multiplicacion de matrices impares(solo una matriz cargada)."
 cat entrada/testImpar1.txt | ./tp0
 echo "-------------------------------------------------------------------------"
-echo "Test 8: Multiplicacion de matrices impares(5 matrices cargadas)"
+echo "Test 8: Multiplicacion de matrices impares(5 matrices cargadas)."
 cat entrada/testImpar2.txt | ./tp0
+echo "-------------------------------------------------------------------------"
+echo "Test 9: Multiplicacion de matrices cuando la cantidad de elementos es manor."
+cat entrada/testFormat3.txt | ./tp0
+echo "-------------------------------------------------------------------------"
+echo "Test 10: Multiplicacion de matrices cuando la cantidad de elementos es menor."
+cat entrada/testFormat4.txt | ./tp0
+echo "-------------------------------------------------------------------------"
+echo "Test 11: Multiplicacion de matrices cuando la cantidad de elementos es mayor(TERMINAR...)."
+cat entrada/testCantElementosIncorrectos.txt | ./tp0
 echo "========================================================================="
 echo "Fin de pruebas"
 echo "========================================================================="
